3a211afcc0af525aa9aa3782d05cce4478cb8977
dmelcer9 <dmelcer9@gmail.com>
2022-10-15 12:53:29 -0400

    Skeleton code

--- stablediffusion_dixit/game_logic/model.py
+++ stablediffusion_dixit/game_logic/model.py
@@ -1,5 +1,7 @@
 import enum
 
+from stablediffusion_dixit.image_generation.local_generation.local_image_generator import LocalImageGenerator
+
 
 class GamePhase(enum.Enum):
     WaitingToStart = 0
@@ -19,10 +21,13 @@ class GamePhase(enum.Enum):
 
 class GameState:
     def __init__(self):
+        self.image_generator = LocalImageGenerator()
+
         self.phase = GamePhase.WaitingToStart
         self.players = []
         self.active_player = None
         self.active_players_image = None
+        self.active_players_image_ticket = None
         self.other_players_images = {}
         self.tvs = []
 
@@ -37,12 +42,25 @@ class GameState:
 
 
     def receive_prompt(self, id, prompt):
-        pass
+        if self.phase == GamePhase.ActivePlayerPrompt:
+            if id == self.active_player:
+                self.active_players_image_ticket = self.image_generator.request_generation(prompt, callback=self.receive_image_finished_generating)
+                self.phase = GamePhase.ActivePlayerImageWait
+
+                # TODO Send messages to everyone to show waiting screen
+
+        elif self.phase == GamePhase.AllPlayersPrompt:
+            pass
 
     def receive_proceed_active_player(self, id):
         pass
 
-    def receive_image_finished_generating(self, image_num):
+    def receive_image_finished_generating(self, image_num, image_path, anim_path):
+        if self.phase == GamePhase.ActivePlayerImageWait:
+            if self.active_players_image_ticket == image_num:
+                self.active_players_image = image_path
+                # TODO Send message to active player to give clue
+                self.phase = GamePhase.ActivePlayerGiveClue
         pass
 
     def receive_vote(self, id, voted_for):

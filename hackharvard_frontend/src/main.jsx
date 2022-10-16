45a27cde71749402d4edfdcae8f6dfeae41f6724
Xzero864 <littlejsilva@gmail.com>
2022-10-15 22:03:41 -0400

    css

--- hackharvard_frontend/src/main.jsx
+++ hackharvard_frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div class_name = "outlined"><App /></div>
   </React.StrictMode>
 )
 
